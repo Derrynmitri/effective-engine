91b9c58b29319d6aba84a36e6a4814415ea259d1
Derryn Mitri <dm.derryn@gmail.com>
2025-05-24 13:28:16 +0200

    Add Devise for user auth

---
+++ test/integration/user_authentication_test.rb
@@ -0,0 +1,21 @@
+require "test_helper"
+
+class UserAuthenticationTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+  end
+
+  test "user can log in and log out" do
+    # Login
+    sign_in @user
+    get root_path 
+    assert_response :success
+    assert_select "a", ref: "logout"
+
+    # Logout
+    sign_out @user
+    get root_path
+    assert_response :success
+    assert_select "a", ref: "login"
+  end
+end
