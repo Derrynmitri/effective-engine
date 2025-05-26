1ef9057d78fd529d0da8859d9d38dac7cded17c2
Derryn Mitri <dm.derryn@gmail.com>
2025-05-26 22:43:23 +0200

    Update match results display and update navbar link for match history

--- app/helpers/match_results_helper.rb
+++ app/helpers/match_results_helper.rb
@@ -1,2 +1,9 @@
 module MatchResultsHelper
+  def match_result_player_name(match_result, colour)
+    if match_result.draw
+      colour == :white ? match_result.match.white_player.full_name : match_result.match.black_player.full_name
+    else
+      colour == :white ? (match_result.winner&.full_name || "N/A") : (match_result.loser&.full_name || "N/A")
+    end
+  end
 end
